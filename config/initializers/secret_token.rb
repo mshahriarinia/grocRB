8db709476bd6274eefec00921af41cb489a363d3
mshahriarinia <info@mshahriarinia.com>
2014-03-16 14:04:35 -0700

    intial rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GrocRB::Application.config.secret_key_base = 'a11bf584b4057cb3dac2a95378023cbd7341a05ca2dbfe27a703f2ed4b6106d1db6cb0efcb32385cfe49dda65af80e22d60dc3bc7a784ba450d6b2886c9001a3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GrocRB::Application.config.session_store :cookie_store, key: '_grocRB_session'
