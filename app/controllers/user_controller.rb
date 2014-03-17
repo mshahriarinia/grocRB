f018cd500a0e869bbdf3541d3b21169c28ed74f9
mshahriarinia <info@mshahriarinia.com>
2014-03-17 12:34:17 -0700

    Copy-paste and refactored stuff from product type for user

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,44 @@
+class UsersController < ApplicationController
+
+  def show
+	id = params[:id]
+	@user = User.find(id)
+  end
+
+  def index
+	@users = User.all
+  end
+
+  def new
+  end
+
+  def create
+	@user = User.create!(user_params)
+	flash[:notice] = "#{@user.name} was successfully created."
+        redirect_to users_path
+  end
+
+  def edit
+	@user = User.find params[:id]
+  end
+
+  def update
+	@user = User.find params[:id]
+	@user.update_attributes!(user_params)
+        flash[:notice] = "#{@user.name} was successfully updated."
+        redirect_to user_path(@user)
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:notice] = "Product type '#{@user.name}' deleted."
+    redirect_to users_path
+  end
+
+  private 
+  def user_params
+    params.required(:user).permit(:name, :password, :email, :description )
+  end
+
+end
