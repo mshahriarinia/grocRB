cf05cf2b198ae8b3ed0b51bba9daa4159ec7295d
mshahriarinia <info@mshahriarinia.com>
2014-03-19 15:43:49 -0700

    Debugged before AUTH

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ActiveRecord::Base
 
   validates :name, :presence => true
   validates :password, :presence => true
-  validates :email, :presence => true, uniqueness: { case_sensitive: false }
+  validates :email, :presence => { message: "field is empty." }
+  validates :email, :uniqueness => { :case_sensitive => false, message: "User is already registered" }
   validates :description, length: { maximum: 2000 }
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
